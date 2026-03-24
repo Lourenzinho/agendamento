f43ce7367a09e969cbac49cf629e824e9f056acc
Graciani021e <graciani021e@gmail.com>
2026-03-24 01:21:39 -0300

    Permissionamento

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+// use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        //Cargos/Roles
+        Role::create(['name' => 'aluno']);
+        Role::create(['name' => 'professor']);
+        Role::create(['name' => 'admin']);
+
+        //Permissões/Permissions
+        Permission::create(['name' => "reservar"]);
+        Permission::create(['name' => "reservar"]);
     }
 }
